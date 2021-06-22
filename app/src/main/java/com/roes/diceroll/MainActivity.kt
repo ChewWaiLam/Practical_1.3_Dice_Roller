878c6d3b03b1e05515c1edd8556b0e84ebfb3a78
Wills Chew <chewwl-wm19@student.tarc.edu.my>
2021-06-22 19:39:51 +0800

    Second Submit:
    Full the requirement:
    - The app layout should include one image view and two buttons.
    - The app's code should set two click handlers, one for each button.
    - The click handler for the Clear button should set the dice image to R.drawable.empty_dice.
    - Coding Challenge (Optional)
    
    Question 1: Which <ImageView> attribute indicates a source image that should be used only in Android Studio?
    - I think is android:srcCompat
    
    Question 2: Which method changes the image resource for an ImageView in Kotlin code?
    - I think is setImageResource()
    
    Question 3: What does the lateinit keyword in a variable declaration indicate in Kotlin code?
    - I think is The variable is only initialized at app runtime.
    
    Question 4: Which Gradle configuration indicates the most recent API level your app has been tested with?
    - I think is compileSdkVersion
    
    Question 5: You see an import line in your code that starts with androidx. What does this mean?
    - I think is The class is part of the Android Jetpack libraries.

--- app/src/main/java/com/roes/diceroll/MainActivity.kt
+++ app/src/main/java/com/roes/diceroll/MainActivity.kt
@@ -3,54 +3,71 @@ package com.roes.diceroll
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    var randomInt = 0
+    var dice1 = 0
+    var dice2 = 0
+    lateinit var diceImage: ImageView
+    lateinit var diceImage2: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val rollbutton: Button = findViewById(R.id.btn_Roll)
-        val btnCountUp: Button = findViewById(R.id.buttonCountUp)
-        val btnReset: Button = findViewById(R.id.buttonReset)
+
+        val btnClear: Button = findViewById(R.id.buttonClear)
 
         rollbutton.setOnClickListener(){
-            RandomInt()
+            dice1 = RandomDice(dice1)
+            dice2 = RandomDice(dice2)
+
+            ShowDice()
         }
 
-        btnCountUp.setOnClickListener(){
-            Increment()
-        }
 
-        btnReset.setOnClickListener(){
-            randomInt = 0
-            ShowString()
+        btnClear.setOnClickListener(){
+            dice1 = 0
+            dice2 = 0
+            ShowDice()
         }
 
     }
 
-    private fun RandomInt() {
-        randomInt = (1..6).random()
-        ShowString()
 
+
+    private fun RandomDice(a: Int): Int {
+        return (1..6).random()
     }
 
-    private fun Increment() {
-        if (randomInt >= 6) {
-            randomInt = 6
-        } else {
-            randomInt += 1
+    private fun ShowDice() {
+        diceImage = findViewById(R.id.DiceImageOne)
+        diceImage2 = findViewById(R.id.DiceImageTwo)
+        val drawableResource = when (dice1) {
+            0 -> R.drawable.empty_dice
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
         }
-        ShowString()
-    }
-
-    private fun ShowString() {
-        val txtResult: TextView = findViewById(R.id.txt_Result)
-        txtResult.text = randomInt.toString()
+        val drawableResource2 = when (dice2) {
+            0 -> R.drawable.empty_dice
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImage.setImageResource(drawableResource)
+        diceImage2.setImageResource(drawableResource2)
     }
 
 }
